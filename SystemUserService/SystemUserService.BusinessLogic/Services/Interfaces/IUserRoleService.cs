63a8267d4baf37f2c7073e24f6f58ed848297963
Viktor Pidhornyy <viktor.podgornyy1@gmail.com>
2024-12-07 15:38:23 +0200

    change create, update, get userRole by userId

--- SystemUserService/SystemUserService.BusinessLogic/Services/Interfaces/IUserRoleService.cs
+++ SystemUserService/SystemUserService.BusinessLogic/Services/Interfaces/IUserRoleService.cs
@@ -4,13 +4,13 @@ using SystemUserService.Common.Results;
 
 namespace SystemUserService.BusinessLogic.Services.Interfaces
 {
-	public interface IUserRoleService
-	{
-		public Task<Result<List<UserRole>>> GetAllUsersRoles();
+    public interface IUserRoleService
+    {
+        public Task<Result<List<UserRole>>> GetAllUsersRoles();
 
-		public Task<Result<List<UserRole>>> GetUserRoleByUserId(int id);
-		public Task<Result<List<UserRole>>> GetUserRoleByRoleId(int id);
-		public Task<Result<List<UserRole>>> CreateUserRole(UserRoleCreateParameters userRoleCreateParam);
-		public Task<Result<List<UserRole>>> UpdateUserRole(UserRoleUpdateParameters userRoleUpdateParam);
-	}
+        public Task<Result<UserRole>> GetUserRoleByUserId(int id);
+        public Task<Result<List<UserRole>>> GetUserRoleByRoleId(int id);
+        public Task<Result<UserRole>> CreateUserRole(UserRoleCreateParameters userRoleCreateParam);
+        public Task<Result<UserRole>> UpdateUserRole(UserRoleUpdateParameters userRoleUpdateParam);
+    }
 }
--- SystemUserService/SystemUserService.DataAccess/Repositories/Intefaces/IUserRoleRepository.cs
+++ SystemUserService/SystemUserService.DataAccess/Repositories/Intefaces/IUserRoleRepository.cs
@@ -3,13 +3,13 @@ using SystemUserService.DataAccess.DTO.UsersRoles;
 
 namespace SystemUserService.DataAccess.Repositories.Intefaces
 {
-	public interface IUserRoleRepository
-	{
-		public Task<Result<List<UserRoleDTO>>> GetAllUsersRoles();
+    public interface IUserRoleRepository
+    {
+        public Task<Result<List<UserRoleDTO>>> GetAllUsersRoles();
 
-		public Task<Result<List<UserRoleDTO>>> GetUserRoleByUserId(int id);
-		public Task<Result<List<UserRoleDTO>>> GetUserRoleByRoleId(int id);
-		public Task<Result> CreateUserRole(int userId, int roleId);
-		public Task<Result> UpdateUserRole(int userRoleId, int userId, int roleId);
-	}
+        public Task<Result<UserRoleDTO>> GetUserRoleByUserId(int id);
+        public Task<Result<List<UserRoleDTO>>> GetUserRoleByRoleId(int id);
+        public Task<Result> CreateUserRole(int userId, int roleId);
+        public Task<Result> UpdateUserRole(int userRoleId, int userId, int roleId);
+    }
 }
