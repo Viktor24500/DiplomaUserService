1f4eb81dc5ac0d63ca5aaeaeb0c80b4159be2679
Viktor Pidhornyy <viktor.podgornyy1@gmail.com>
2024-12-11 21:55:27 +0200

    change kestrel port from 5000 to 5003

--- SystemUserService/SystemUserService/Request/UserRole/UserRoleCreateRequest.cs
+++ SystemUserService/SystemUserService/Request/UserRole/UserRoleCreateRequest.cs
@@ -1,13 +1,13 @@
 ﻿namespace SystemUserService.Request.UserRole
 {
-	public class UserRoleCreateRequest
-	{
-		public UserRoleCreateRequest(int userId, int roleId)
-		{
-			UserId = userId;
-			RoleId = roleId;
-		}
-		public int UserId { get; set; }
-		public int RoleId { get; set; }
-	}
+    public class UserRoleCreateRequest
+    {
+        public UserRoleCreateRequest(int userId, int roleId)
+        {
+            UserId = userId;
+            RoleId = roleId;
+        }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
 }
